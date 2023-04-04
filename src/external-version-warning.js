eb4c4351c0d3bf499af374efb7f93bdd20c8ec85
Anthony Johnson <aj@ohess.org>
2023-04-04 16:34:25 -0700

    Lit web component proof of concept

--- src/external-version-warning.js
+++ src/external-version-warning.js
@@ -1,73 +1,123 @@
 import { library, icon } from "@fortawesome/fontawesome-svg-core";
 import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
-import { html, render } from "lit";
+import { html, render, LitElement } from "lit";
 
-import styles from "./external-version-warning.css";
+import styleSheet from "./external-version-warning.css";
+
+export class ExternalVersionBannerElement extends LitElement {
+  static properties = {
+    floating: {},
+
+    config: {},
+    urls: {},
+  };
+
+  static styles = styleSheet;
+
+  constructor() {
+    super();
+
+    this.floating = true;
+    this.config = {};
+    this.urls = {
+      build: null,
+      external: null,
+    };
+  }
+
+  static fromConfig(config) {
+    const elem = new ExternalVersionBannerElement();
+    elem.config = config;
+    // TODO the URLs here should come from a backend API, instead of manually
+    // created here.
+    elem.urls = {
+      build: `${window.location.protocol}//${config.domains.dashboard}/projects/${config.project.slug}/builds/${config.build.id}/`,
+      external: `${config.project.repository_url}/pull/${config.version.slug}`,
+    };
+    return elem;
+  }
+
+  render() {
+    library.add(faCircleXmark);
+    const xmark = icon(faCircleXmark, {
+      title: "Close",
+    });
+
+    const bannerClass = this.floating ? "floating" : "";
+
+    return html`
+<div class="${bannerClass}" @click=${this.closeBanner}>
+  <div class="title">
+    Warning
+    <div class="right">
+      ${xmark.node[0]}
+    </div>
+  </div>
+  <p>
+    This page
+    <a href="${this.urls.build}">was created</a>
+    from a pull request
+    (<a href="${this.urls.external}">#${this.config.version.slug}</a>).
+  </p>
+</div>`;
+  }
+
+  closeBanner(e) {
+    // TODO add cookie to allow closing this banner for all page views on this
+    // PR build.
+    this.remove();
+  }
+}
 
 /**
  * Inject a warning informing the documentation comes from an external version (e.g. pull request)
- *
  */
 export function injectExternalVersionWarning(config) {
-  // TODO: make all these banners (injected HTML) templates that users can override with their own.
-  // This way, we allow customization of the look&feel without compromising the logic.
-  if (
-    !config.features.external_version_warning.enabled ||
-    !config.version.external
-  ) {
-    return false;
+  // TODO drop this function and move this logic to index.js instead. This
+  // function can go away once all addons share a similar interface for common
+  // logic, like checking if an addon is needed.
+  if (ExternalVersionWarningAddon.is_enabled) {
+    return new ExternalVersionWarningAddon(config);
+  }
+}
+
+/**
+  * External (pull request) version warning addon
+  *
+  * The default implementation is a floating element, but this can also be hard
+  * coded into the page from the author or theme author. If there is a hardcoded
+  * element, we do not inject a new element, but the web component is
+  * initialized as normal by the browser.
+  *
+  * @param {Object} config - Addon configuration object
+  */
+export class ExternalVersionWarningAddon {
+  constructor(config) {
+    customElements.define('readthedocs-banner', ExternalVersionBannerElement);
+    const elems = document.querySelector('readthedocs-banner');
+    if (!elems) {
+      this.banner = ExternalVersionBannerElement.fromConfig(config);
+      render(this.banner, document.body);
+    }
   }
 
-  return new Promise((resolve, reject) => {
-    // BUG resolve, reject are never called in this function
-    document.adoptedStyleSheets.push(styles);
-    library.add(faCircleXmark);
+  // TODO: make all these banners (injected HTML) templates that users can override with their own.
+  // This way, we allow customization of the look&feel without compromising the logic.
+  // Allow to override the admonition template
+  // if (config.features.banner.external.template) {
+  //     admonition = config.features.banner.external.template;
+  // }
 
-    let admonition;
-
-    const xmark = icon(faCircleXmark, {
-      title: "Close",
-      classes: ["close-icon"],
-    });
-    const clickClose = (event) => {
-      // Remove the event the event listener was added on
-      event.currentTarget.remove();
-    };
-
-    // TODO the URLs here should come from a backend API, instead of manually
-    // created here.
-    admonition = html`
-      <div class="readthedocs-banner version-warning" @click=${clickClose}>
-        <div class="title">
-          Warning
-          <div class="right">${xmark.node[0]}</div>
-        </div>
-        <p>
-          This page
-          <a
-            href="${window.location.protocol}//${config.domains
-              .dashboard}/projects/${config.project.slug}/builds/${config.build
-              .id}/"
-            >was created</a
-          >
-          from a pull request (<a
-            href="${config.project.repository_url}/pull/${config.version.slug}"
-            >#${config.version.slug}</a
-          >).
-        </p>
-      </div>
-    `;
-
-    // Allow to override the admonition template
-    // if (config.features.banner.external.template) {
-    //     admonition = config.features.banner.external.template;
-    // }
-
-    try {
-      render(admonition, document.body);
-      resolve();
-    } catch {
-      reject();
-    }
-  });
+  /**
+    * Test if addon is enabled in the configuration
+    *
+    * @param {Object} config - Addon configuration object
+    */
+  static is_enabled(config) {
+    return (
+      config.features &&
+      config.features.external_version_warning.enabled &&
+      config.version.external
+    );
+  }
 }
