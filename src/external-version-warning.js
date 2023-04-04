9867509d9e3884afe221bf2e3e8c245f2ba3a262
Anthony Johnson <aj@ohess.org>
2023-04-04 13:44:33 -0700

    Reduce CSS/JS for version warning

--- src/external-version-warning.js
+++ src/external-version-warning.js
@@ -17,29 +17,39 @@ export function injectExternalVersionWarning(config) {
   }
 
   return new Promise((resolve, reject) => {
+    // BUG resolve, reject are never called in this function
     document.adoptedStyleSheets.push(styles);
     library.add(faCircleXmark);
 
     const xmark = icon(faCircleXmark, {
       title: "Close",
-      classes: ["readthedocs-external-version-warning-close-icon"],
+      classes: ["close-icon"],
     });
+    const elem_xmark = xmark.node[0];
 
+    // Attach click event directly, not through jQuery
+    // TODO this does not play well with HTML templates as strings yet
+    elem_xmark.onclick = function () {
+      adominition.style.display = "none";
+    };
+
+    // TODO the URLs here should come from a backend API, instead of manually
+    // created here.
+    // TODO the pull request URL likely 
     const admonition = `
-<div id="readthedocs-external-version-warning">
-<div class="readthedocs-external-version-warning-shadow"></div>
-<div class="readthedocs-external-version-warning-container">
-  <div class="readthedocs-external-version-warning-title">
-    <p>Warning</p>
-    ${xmark.html[0]}
+<div class="readthedocs-banner version-warning">
+  <div class="title">
+    Warning
+    <div class="right">
+      ${elem_xmark.outerHTML}
+    </div>
   </div>
-    <p>
-      This page
-      <a href="${window.location.protocol}//${config.domains.dashboard}/projects/${config.project.slug}/builds/${config.build.id}/">was created</a>
-      from a pull request
-      (<a href="${config.project.repository_url}/pull/${config.version.slug}">#${config.version.slug}</a>).
-    </p>
-  </div>
+  <p>
+    This page
+    <a href="${window.location.protocol}//${config.domains.dashboard}/projects/${config.project.slug}/builds/${config.build.id}/">was created</a>
+    from a pull request
+    (<a href="${config.project.repository_url}/pull/${config.version.slug}">#${config.version.slug}</a>).
+  </p>
 </div>
 `;
 
@@ -49,15 +59,5 @@ export function injectExternalVersionWarning(config) {
     // }
 
     document.body.insertAdjacentHTML("beforeend", admonition);
-
-    // Connect the X to close the banner
-    const close = document.querySelector(
-      "#readthedocs-external-version-warning .readthedocs-external-version-warning-close-icon"
-    );
-    close.onclick = function () {
-      document.querySelector(
-        "#readthedocs-external-version-warning"
-      ).style.display = "none";
-    };
   });
 }
