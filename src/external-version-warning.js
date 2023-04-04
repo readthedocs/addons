92f0e494901c7c2639ac6d5abc8530cb227cc4f5
Anthony Johnson <aj@ohess.org>
2023-04-04 14:25:58 -0700

    Show lightweight Lit usage

--- src/external-version-warning.js
+++ src/external-version-warning.js
@@ -1,6 +1,8 @@
-import styles from "./external-version-warning.css";
 import { library, icon } from "@fortawesome/fontawesome-svg-core";
 import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import { html, render } from "lit";
+
+import styles from "./external-version-warning.css";
 
 /**
  * Inject a warning informing the documentation comes from an external version (e.g. pull request)
@@ -21,43 +23,51 @@ export function injectExternalVersionWarning(config) {
     document.adoptedStyleSheets.push(styles);
     library.add(faCircleXmark);
 
+    let admonition;
+
     const xmark = icon(faCircleXmark, {
       title: "Close",
       classes: ["close-icon"],
     });
-    const elem_xmark = xmark.node[0];
-
-    // Attach click event directly, not through jQuery
-    // TODO this does not play well with HTML templates as strings yet
-    elem_xmark.onclick = function () {
-      adominition.style.display = "none";
+    const clickClose = (event) => {
+      // Remove the event the event listener was added on
+      event.currentTarget.remove();
     };
 
     // TODO the URLs here should come from a backend API, instead of manually
     // created here.
-    // TODO the pull request URL likely
-    const admonition = `
-<div class="readthedocs-banner version-warning">
-  <div class="title">
-    Warning
-    <div class="right">
-      ${elem_xmark.outerHTML}
-    </div>
-  </div>
-  <p>
-    This page
-    <a href="${window.location.protocol}//${config.domains.dashboard}/projects/${config.project.slug}/builds/${config.build.id}/">was created</a>
-    from a pull request
-    (<a href="${config.project.repository_url}/pull/${config.version.slug}">#${config.version.slug}</a>).
-  </p>
-</div>
-`;
+    admonition = html`
+      <div class="readthedocs-banner version-warning" @click=${clickClose}>
+        <div class="title">
+          Warning
+          <div class="right">${xmark.node[0]}</div>
+        </div>
+        <p>
+          This page
+          <a
+            href="${window.location.protocol}//${config.domains
+              .dashboard}/projects/${config.project.slug}/builds/${config.build
+              .id}/"
+            >was created</a
+          >
+          from a pull request (<a
+            href="${config.project.repository_url}/pull/${config.version.slug}"
+            >#${config.version.slug}</a
+          >).
+        </p>
+      </div>
+    `;
 
     // Allow to override the admonition template
     // if (config.features.banner.external.template) {
     //     admonition = config.features.banner.external.template;
     // }
 
-    document.body.insertAdjacentHTML("beforeend", admonition);
+    try {
+      render(admonition, document.body);
+      resolve();
+    } catch {
+      reject();
+    }
   });
 }
