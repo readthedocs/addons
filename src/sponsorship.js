9efc6ece2c0509ef722e6c8ea419d6808b8e02f9
Manuel Kaufmann <humitos@gmail.com>
2023-07-12 19:07:15 +0200

    Format

--- src/sponsorship.js
+++ src/sponsorship.js
@@ -74,6 +74,10 @@ export class SponsorshipAddon extends AddonBase {
   }
 
   static isEnabled(config) {
-    return config.addons && config.addons.ethicalads.enabled && !config.addons.ethicalads.ad_free;
+    return (
+      config.addons &&
+      config.addons.ethicalads.enabled &&
+      !config.addons.ethicalads.ad_free
+    );
   }
 }
