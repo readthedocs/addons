e056b2e478e26e09da5cf47d49f3645cd121a041
Anthony Johnson <aj@ohess.org>
2023-09-13 15:01:05 -0700

    Simplify wtr rollup for css, json, and svg imports

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,6 +1,9 @@
 import { importMapsPlugin } from '@web/dev-server-import-maps';
-import { fromRollup } from '@web/dev-server-rollup';
+import { fromRollup, rollupAdapter } from '@web/dev-server-rollup';
 import rollupCommonjs from '@rollup/plugin-commonjs';
+import rollupImage from '@rollup/plugin-image';
+import rollupJson from '@rollup/plugin-json';
+import rollupLitCss from 'rollup-plugin-lit-css';
 
 const pluginCommonjs = fromRollup(rollupCommonjs);
 
@@ -9,22 +12,15 @@ export default {
   files: ['./tests/**/*.test.{js,html}'],
   nodeResolve: {exportConditions: ['development']},
   preserveSymlinks: true,
+  mimeTypes: {
+    '**/*.css': 'js',
+    '**/*.svg': 'js',
+    '**/*.json': 'js',
+  },
   plugins: [
-    importMapsPlugin({
-      inject: {
-        importMap: {
-          imports: {
-            // These files are imported through Webpack, but don't resolve the
-            // same way when using Rollup, which is used by WTR in testing. We
-            // don't need to test these files, so just mock the output files.
-            '/src/images/logo-wordmark-dark.svg': '/tests/__mocks__/stylesheet.js',
-            '/src/notification.css': '/tests/__mocks__/stylesheet.js',
-            '/src/search.css': '/tests/__mocks__/stylesheet.js',
-            '/package.json': '/tests/__mocks__/placeholder.js',
-          },
-        },
-      },
-    }),
+    rollupAdapter(rollupJson()),
+    rollupAdapter(rollupImage()),
+    rollupAdapter(rollupLitCss()),
     pluginCommonjs({
       include: [],
     }),
