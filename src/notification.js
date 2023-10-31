cd6f654ab55adb3f79afc32ac47d5753fa0e3a79
Manuel Kaufmann <humitos@gmail.com>
2023-10-31 09:05:50 +0100

    Notification: typos on messages (#178)
    Reported by @jertel in
    
    https://github.com/readthedocs/addons/pull/174#pullrequestreview-1703974771
    
    ---------
    
    Co-authored-by: Anthony <aj@ohess.org>

--- src/notification.js
+++ src/notification.js
@@ -144,7 +144,7 @@ export class NotificationElement extends LitElement {
             </a>
           </div>
           <div class="content">
-            Some features may not be yet available in the publised stable
+            Some features may not yet be available in the published stable
             version. Read the
             <a href="${this.urls.stable}"
               >stable version of this documentation</a
@@ -170,7 +170,7 @@ export class NotificationElement extends LitElement {
             </a>
           </div>
           <div class="content">
-            You may be reading and old version of this documentation. Read the
+            You may be reading an old version of this documentation. Read the
             <a href="${this.urls.stable}"
               >latest stable version of this documentation</a
             >.
