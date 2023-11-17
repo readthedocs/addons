ae9f9296b28fb3d934f56f018d61921fbfca5770
Manuel Kaufmann <humitos@gmail.com>
2023-11-17 12:27:30 +0100

    Test: EthicalAds HTML content checks (#191)
    Initial tests for auto-injected ad placement.

---
+++ tests/__snapshots__/ethicalads.test.snap.js
@@ -0,0 +1,13 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["EthicalAd addon ad placement defined by the user"] = `<div
+  class="ad-flat ethical-alabaster"
+  data-ea-manual="true"
+  data-ea-publisher="readthedocs"
+  data-ea-type="readthedocs-sidebar"
+  id="ethical-ad-placement"
+>
+</div>
+`;
+/* end snapshot EthicalAd addon ad placement defined by the user */
