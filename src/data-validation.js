bedbb71a2c801eabba2da8a4f458825f22eb2cb4
Manuel Kaufmann <humitos@gmail.com>
2023-11-22 13:23:24 +0100

    API: use `version_schema` instead of `single_version`
    The backend API response changed and `single_version` is deprecated now.
    
    Closes #195

--- src/data-validation.js
+++ src/data-validation.js
@@ -167,10 +167,15 @@ const addons_flyout = {
       properties: {
         current: {
           type: "object",
-          required: ["slug", "single_version"],
+          required: ["slug", "versioning_schema"],
           properties: {
             slug: { type: "string" },
-            single_version: { type: "boolean" },
+            versioning_schema: {
+              enum: [
+                "multiple_versions_with_translations",
+                "single_version_without_translations",
+              ],
+            },
           },
         },
       },
@@ -276,7 +281,12 @@ const addons_notifications = {
           type: "object",
           properties: {
             slug: { type: "string" },
-            single_version: { type: "boolean" },
+            versioning_schema: {
+              enum: [
+                "multiple_versions_with_translations",
+                "single_version_without_translations",
+              ],
+            },
             // TODO: use ajv-formats URI type
             repository: {
               type: "object",
