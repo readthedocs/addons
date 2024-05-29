42d6b44f4f547fb4b418c70b49efc14bd12a023a
Manuel Kaufmann <humitos@gmail.com>
2024-05-29 19:36:30 +0200

    Update src/ethicalads.js
    Co-authored-by: Eric Holscher <25510+ericholscher@users.noreply.github.com>

--- src/ethicalads.js
+++ src/ethicalads.js
@@ -297,6 +297,7 @@ export class EthicalAdsAddon extends AddonBase {
   }
 
   addWidthListener() {
+    // Hide ad when the window is smaller than the stickybox width
     const placement = document.querySelector("#readthedics-ea");
     if (placement && placement.data.eaType === "stickybox") {
       window.addEventListener("resize", function () {
