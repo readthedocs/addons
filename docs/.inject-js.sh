c9acbaed2d7561bfa7ea456b26b5f0d2c73b93f1
Manuel Kaufmann <humitos@gmail.com>
2024-05-29 16:59:46 +0200

    Delete old file

--- docs/.inject-js.sh
+++
@@ -1,2 +0,0 @@
-sed -i -e 's|</head>|<script src="../readthedocs-addons.js"></script></head>|g' $READTHEDOCS_OUTPUT/html/examples/pelican/index.html
-sed -i -e 's|</head>|<script src="../readthedocs-addons.js"></script></head>|g' $READTHEDOCS_OUTPUT/html/examples/sphinx/index.html
