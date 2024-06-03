94f1d139c6564ce03557c3c107d646c5399e93dc
Manuel Kaufmann <humitos@gmail.com>
2024-06-03 12:42:36 +0200

    Docs: add environment variable (#337)
    The theme should include this into the `html_context` automatically, but
    for some reason this is not working like that.

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,13 @@
+import os
+
 project = 'Read the Docs Addons'
 extensions = []
 html_theme = 'furo'
+
+html_context = {}
+
+# NOTE: this should be done automatically by the theme,
+# but for some reason it's not working as I expect.
+# https://github.com/pradyunsg/furo/blob/0439e84b3c2c5b168f22ea2e93c867fd37911dfa/docs/conf.py#L134
+if "READTHEDOCS" in os.environ:
+    html_context["READTHEDOCS"] = True
