c4f2142f1eb80d721de5d7e6f2a8b684f4d81d4b
Žan Anderle <zan.anderle@gmail.com>
2024-09-19 14:20:00 +0200

    Replace tippy with floating-ui

--- src/tooltips.js
+++ src/tooltips.js
@@ -1,70 +1,93 @@
-import tippy from "tippy.js";
-import styles from "tippy.js/dist/tippy.css";
-import lightTheme from "tippy.js/themes/light.css";
+import styles from "./tooltips.css";
+
 import { domReady, CLIENT_VERSION } from "./utils";
+import { computePosition, flip, shift, offset } from "@floating-ui/dom";
 
 export function initializeTooltips(config) {
-  // Inject our styles for the flyout
-  document.adoptedStyleSheets.push(styles);
-  document.adoptedStyleSheets.push(lightTheme);
+
 
   domReady.then(() => {
+    // Inject our styles for the tooltips
+    document.adoptedStyleSheets.push(styles);
     // TODO: decide what's the correct selector.
     // Our Sphinx extension is adding a class depending on the configuration.
     // However, we won't have this for other doctools or when the extension is not installed.
     const elements = document.querySelectorAll("[role=main] a.internal"); // .each(function () { this.removeAttr('title'); });
-    tippy(elements, {
-      content: "Loading...",
-      allowHTML: true,
-      arrow: true,
-      hideOnClick: false,
-      interactive: true,
-      theme: "light",
-      placement: "auto",
-      maxWidth: 550,
-      // TODO: make the content of the tooltip scrollable
-      onShow(instance) {
-        const href = instance.reference.href;
+    elements.forEach(el => el.addEventListener("mouseenter", showTooltip));
+    elements.forEach(el => el.addEventListener("mouseleave", hideTooltip));
 
-        // we set a variable so the data is only loaded once via Ajax, not every time the tooltip opens
-        if (instance.reference.getAttribute("loaded") !== "true") {
-          const hrefAttribute =
-            "http://docs.devthedocs.org/en/stable/tutorial/index.html#modifying-versions";
-          // "http://docs.devthedocs.org/en/stable/tutorial/index.html#upgrading-the-python-version";
-          // "http://docs.devthedocs.org/en/stable/tutorial/index.html";
-          const url = getEmbedURL(hrefAttribute);
-
-          fetch(url, {
-            method: "GET",
-            headers: {
-              "X-RTD-Hosting-Integrations-Version": CLIENT_VERSION,
-            },
-          })
-            .then((response) => {
-              if (!response.ok) {
-                throw new Error("Error hitting Read the Docs embed API");
-              }
-              return response.json();
-            })
-            .then((data) => {
-              // call the 'content' method to update the content of our tooltip with the returned data.
-              // note: this content update will trigger an update animation (see the updateAnimation option)
-              // TODO: decide whether or not to truncate the content
-              // Do we want to have "modals" as well? are those useful?
-              // const content = truncateContent(data["content"]);
-              const content = data["content"];
-              instance.setContent(content);
-
-              // to remember that the data has been loaded
-              instance.reference.setAttribute("loaded", "true");
-            })
-            .catch((error) => {
-              console.log(error.message);
-            });
-        }
-      },
-    });
   });
+}
+
+function getRelatedTooltip(anchorElement) {
+  const existingTooltip = anchorElement.parentElement.querySelector(`div[data-tooltip-href="${anchorElement.href}"]`);
+  if (existingTooltip) {
+    return existingTooltip;
+  }
+  else {
+    const newTooltip = document.createElement("div");
+    newTooltip.insertAdjacentHTML("afterbegin", '<div class="tooltip-content">Loading...</div>');
+    newTooltip.setAttribute("data-tooltip-href", anchorElement.href);
+    newTooltip.classList.add("tooltip");
+    anchorElement.insertAdjacentElement("afterend", newTooltip);
+    return newTooltip;
+  }
+}
+
+function showTooltip(el) {
+  const anchorElement = el.target;
+
+  const newTooltip = getRelatedTooltip(anchorElement);
+
+  if (newTooltip !== undefined) {
+    const href = anchorElement.href;
+
+    // we set a variable so the data is only loaded once via Ajax, not every time the tooltip opens
+      const hrefAttribute =
+        "http://docs.devthedocs.org/en/stable/tutorial/index.html#modifying-versions";
+      // "http://docs.devthedocs.org/en/stable/tutorial/index.html#upgrading-the-python-version";
+      // "http://docs.devthedocs.org/en/stable/tutorial/index.html";
+      const url = getEmbedURL(hrefAttribute);
+
+      fetch(url, {
+        method: "GET",
+        headers: {
+          "X-RTD-Hosting-Integrations-Version": CLIENT_VERSION,
+        },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Error hitting Read the Docs embed API");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          // call the 'content' method to update the content of our tooltip with the returned data.
+          // note: this content update will trigger an update animation (see the updateAnimation option)
+          // TODO: decide whether or not to truncate the content
+          // Do we want to have "modals" as well? are those useful?
+          // const content = truncateContent(data["content"]);
+          const content = data["content"];
+          newTooltip.firstChild.innerHTML = content;
+          newTooltip.style.display = 'block';
+          computePosition(anchorElement, newTooltip, {placement: "right", middleware: [offset(10), flip(), shift(20)]}).then(({x, y}) => {
+            Object.assign(newTooltip.style, {
+              left: `${x}px`,
+              top: `${y}px`,
+            });
+          });
+          newTooltip.classList.remove("hide");
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+  }
+}
+
+function hideTooltip(el) {
+  const anchorElement = el.fromElement;
+  const newTooltip = getRelatedTooltip(anchorElement);
+  newTooltip.style.display = '';
 }
 
 function getEmbedURL(url) {
@@ -76,6 +99,8 @@ function getEmbedURL(url) {
     url: url,
   };
 
-  const api_url = "/_/api/v3/embed/?" + new URLSearchParams(params).toString();
+  // const api_url = "https://docs.readthedocs.io/_/api/v3/embed/?doctool=sphinx&doctoolversion=7.3.7&url=https%3A%2F%2Fdocs.readthedocs.io%2Fen%2Fstable%2Fversions.html%23how-we-envision-versions-working";
+  // const api_url = "https://sphinx-hoverxref.readthedocs.io/_/api/v3/embed/?doctool=sphinx&doctoolversion=7.4.7&url=https%3A%2F%2Fsphinx-hoverxref.readthedocs.io%2Fen%2Flatest%2Fhoverxref.html%23hoverxref";
+  const api_url = "https://sphinx-hoverxref.readthedocs.io/_/api/v3/embed/?doctool=sphinx&doctoolversion=7.4.7&url=https%3A%2F%2Fwww.sphinx-doc.org%2Fen%2Fmaster%2Findex.html";
   return api_url;
 }
