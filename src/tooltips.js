539b625ea914baa8d155e680d38c8efe7b69e451
Žan Anderle <zan.anderle@gmail.com>
2024-10-08 16:05:27 +0200

    Implement basic behaviour for the tooltip

--- src/tooltips.js
+++ src/tooltips.js
@@ -1,41 +1,76 @@
 import styles from "./tooltips.css";
 
 import { domReady, CLIENT_VERSION } from "./utils";
-import { computePosition, flip, shift, offset } from "@floating-ui/dom";
+import { computePosition, flip, shift, offset, arrow } from "@floating-ui/dom";
+
+const SHOW_TOOLTIP_DELAY = 1000;
+const HIDE_TOOLTIP_DELAY = 500;
 
 export function initializeTooltips(config) {
-
-
   domReady.then(() => {
     // Inject our styles for the tooltips
     document.adoptedStyleSheets.push(styles);
     // TODO: decide what's the correct selector.
     // Our Sphinx extension is adding a class depending on the configuration.
     // However, we won't have this for other doctools or when the extension is not installed.
-    const elements = document.querySelectorAll("[role=main] a.internal"); // .each(function () { this.removeAttr('title'); });
-    elements.forEach(el => el.addEventListener("mouseenter", showTooltip));
-    elements.forEach(el => el.addEventListener("mouseleave", hideTooltip));
-
+    const elements = document.querySelectorAll("[role=main] a.internal");
+    elements.forEach((el) => el.addEventListener("mouseenter", delayShowTooltip));
+    elements.forEach((el) => el.addEventListener("mouseleave", (ev) => {
+      const tooltip = getRelatedTooltip(ev.target);
+      delayHideTooltip(tooltip);
+    }));
   });
 }
 
 function getRelatedTooltip(anchorElement) {
-  const existingTooltip = anchorElement.parentElement.querySelector(`div[data-tooltip-href="${anchorElement.href}"]`);
+  // Get or create the tooltip that's a sibling to the provided anchor element
+  const existingTooltip = anchorElement.parentElement.querySelector(
+    `div[data-tooltip-href="${anchorElement.href}"]`,
+  );
   if (existingTooltip) {
     return existingTooltip;
-  }
-  else {
+  } else {
     const newTooltip = document.createElement("div");
-    newTooltip.insertAdjacentHTML("afterbegin", '<div class="tooltip-content">Loading...</div>');
+    newTooltip.insertAdjacentHTML("afterbegin", '<div class="arrow"></div>');
+    newTooltip.insertAdjacentHTML(
+      "afterbegin",
+      '<div class="tooltip-content">Loading...</div>',
+    );
     newTooltip.setAttribute("data-tooltip-href", anchorElement.href);
     newTooltip.classList.add("tooltip");
     anchorElement.insertAdjacentElement("afterend", newTooltip);
+    newTooltip.addEventListener("mouseenter", cancelHideDelay);
+    newTooltip.addEventListener("mouseleave", (ev) => delayHideTooltip(newTooltip));
     return newTooltip;
   }
 }
 
-function showTooltip(el) {
-  const anchorElement = el.target;
+let showTooltipTimeoutId = null;
+let hideTooltipTimeoutId = null;
+
+function delayShowTooltip(el) {
+  if (showTooltipTimeoutId === null) {
+    showTooltipTimeoutId = setTimeout(() => { showTooltip(el); }, SHOW_TOOLTIP_DELAY);
+  }
+
+  cancelHideDelay();
+}
+
+function cancelHideDelay() {
+  if (hideTooltipTimeoutId !== null) {
+    clearTimeout(hideTooltipTimeoutId);
+    hideTooltipTimeoutId = null;
+  }
+}
+
+function delayHideTooltip(tooltip) {
+  if (hideTooltipTimeoutId === null) {
+    hideTooltipTimeoutId = setTimeout(() => { hideTooltip(tooltip); }, HIDE_TOOLTIP_DELAY);
+  }
+}
+
+function showTooltip(ev) {
+  const anchorElement = ev.target;
 
   const newTooltip = getRelatedTooltip(anchorElement);
 
@@ -43,51 +78,75 @@ function showTooltip(el) {
     const href = anchorElement.href;
 
     // we set a variable so the data is only loaded once via Ajax, not every time the tooltip opens
-      const hrefAttribute =
-        "http://docs.devthedocs.org/en/stable/tutorial/index.html#modifying-versions";
-      // "http://docs.devthedocs.org/en/stable/tutorial/index.html#upgrading-the-python-version";
-      // "http://docs.devthedocs.org/en/stable/tutorial/index.html";
-      const url = getEmbedURL(hrefAttribute);
+    const hrefAttribute =
+      "http://docs.devthedocs.org/en/stable/tutorial/index.html#modifying-versions";
+    // "http://docs.devthedocs.org/en/stable/tutorial/index.html#upgrading-the-python-version";
+    // "http://docs.devthedocs.org/en/stable/tutorial/index.html";
+    const url = getEmbedURL(hrefAttribute);
 
-      fetch(url, {
-        method: "GET",
-        headers: {
-          "X-RTD-Hosting-Integrations-Version": CLIENT_VERSION,
-        },
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "X-RTD-Hosting-Integrations-Version": CLIENT_VERSION,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error hitting Read the Docs embed API");
+        }
+        return response.json();
       })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Error hitting Read the Docs embed API");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          // call the 'content' method to update the content of our tooltip with the returned data.
-          // note: this content update will trigger an update animation (see the updateAnimation option)
-          // TODO: decide whether or not to truncate the content
-          // Do we want to have "modals" as well? are those useful?
-          // const content = truncateContent(data["content"]);
-          const content = data["content"];
-          newTooltip.firstChild.innerHTML = content;
-          newTooltip.style.display = 'block';
-          computePosition(anchorElement, newTooltip, {placement: "right", middleware: [offset(10), flip(), shift(20)]}).then(({x, y}) => {
-            Object.assign(newTooltip.style, {
-              left: `${x}px`,
-              top: `${y}px`,
-            });
+      .then((data) => {
+        // TODO: decide whether or not to truncate the content
+        // Do we want to have "modals" as well? are those useful?
+        const content = data["content"];
+        newTooltip.firstChild.innerHTML = content;
+        newTooltip.style.display = "block";
+        const arrowElement = newTooltip.querySelector(".arrow");
+        computePosition(anchorElement, newTooltip, {
+          placement: "right",
+          middleware: [
+            offset(10),
+            flip(),
+            shift(20),
+            arrow({ element: arrowElement }),
+          ],
+        }).then(({ x, y, middlewareData, placement }) => {
+          Object.assign(newTooltip.style, {
+            left: `${x}px`,
+            top: `${y}px`,
           });
-          newTooltip.classList.remove("hide");
-        })
-        .catch((error) => {
-          console.log(error.message);
+          const {x: arrowX, y: arrowY} = middlewareData.arrow;
+          const staticSide = {
+            top: 'bottom',
+            right: 'left',
+            bottom: 'top',
+            left: 'right',
+          }[placement.split('-')[0]];
+
+          Object.assign(arrowElement.style, {
+            left: arrowX != null ? `${arrowX}px` : '',
+            top: arrowY != null ? `${arrowY}px` : '',
+            right: '',
+            bottom: '',
+            [staticSide]: '-4px',
+          });
         });
+        newTooltip.classList.remove("hide");
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
   }
 }
 
-function hideTooltip(el) {
-  const anchorElement = el.fromElement;
-  const newTooltip = getRelatedTooltip(anchorElement);
-  newTooltip.style.display = '';
+function hideTooltip(tooltip) {
+  if (showTooltipTimeoutId !== null) {
+    clearTimeout(showTooltipTimeoutId);
+    showTooltipTimeoutId = null;
+  }
+
+  tooltip.style.display = "";
 }
 
 function getEmbedURL(url) {
@@ -101,6 +160,7 @@ function getEmbedURL(url) {
 
   // const api_url = "https://docs.readthedocs.io/_/api/v3/embed/?doctool=sphinx&doctoolversion=7.3.7&url=https%3A%2F%2Fdocs.readthedocs.io%2Fen%2Fstable%2Fversions.html%23how-we-envision-versions-working";
   // const api_url = "https://sphinx-hoverxref.readthedocs.io/_/api/v3/embed/?doctool=sphinx&doctoolversion=7.4.7&url=https%3A%2F%2Fsphinx-hoverxref.readthedocs.io%2Fen%2Flatest%2Fhoverxref.html%23hoverxref";
-  const api_url = "https://sphinx-hoverxref.readthedocs.io/_/api/v3/embed/?doctool=sphinx&doctoolversion=7.4.7&url=https%3A%2F%2Fwww.sphinx-doc.org%2Fen%2Fmaster%2Findex.html";
+  const api_url =
+    "https://sphinx-hoverxref.readthedocs.io/_/api/v3/embed/?doctool=sphinx&doctoolversion=7.4.7&url=https%3A%2F%2Fwww.sphinx-doc.org%2Fen%2Fmaster%2Findex.html";
   return api_url;
 }
