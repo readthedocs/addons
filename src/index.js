0b605f6240e340a85bea95133ab540880424c3ec
Manuel Kaufmann <humitos@gmail.com>
2024-11-18 16:48:03 +0100

    New `CustomScript` addon (#431)
    This is a new addon that allow users to inject a specific JavaScript
    file to all the versions to all the documentation pages to allow them to
    fix issues in old "frozen" (not able to re-build) versions.
    
    Requires https://github.com/readthedocs/readthedocs.org/pull/11758

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import * as ethicalads from "./ethicalads";
 import * as hotkeys from "./hotkeys";
 import * as linkpreviews from "./linkpreviews";
 import * as filetreediff from "./filetreediff";
+import * as customscript from "./customscript";
 import {
   domReady,
   IS_PRODUCTION,
@@ -26,6 +27,7 @@ export function setup() {
     hotkeys.HotKeysAddon,
     linkpreviews.LinkPreviewsAddon,
     filetreediff.FileTreeDiffAddon,
+    customscript.CustomScriptAddon,
   ];
 
   return new Promise((resolve) => {
