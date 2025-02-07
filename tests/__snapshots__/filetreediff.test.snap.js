604010420320b5ec98f7b290940f040ff11bc70b
Manuel Kaufmann <humitos@gmail.com>
2025-02-07 09:56:40 +0100

    Filetreediff: small test suite  (#524)
    Copy the tests done in #523

---
+++ tests/__snapshots__/filetreediff.test.snap.js
@@ -0,0 +1,34 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["Filetreediff tests snapshot filetreediff completely"] = 
+`<div>
+  <div>
+    <label>
+      <input type="checkbox">
+      Show diff
+    </label>
+    <select>
+      <option
+        disabled=""
+        selected=""
+        value=""
+      >
+        Files changed
+      </option>
+      <optgroup label="Added">
+        <option value="http://project.readthedocs.io/en/latest/testing.html">
+          + testing.html
+        </option>
+      </optgroup>
+      <optgroup label="Changed">
+        <option value="http://project.readthedocs.io/en/latest/commercial.html">
+          ± commercial.html
+        </option>
+      </optgroup>
+    </select>
+  </div>
+</div>
+`;
+/* end snapshot Filetreediff tests snapshot filetreediff completely */
+
